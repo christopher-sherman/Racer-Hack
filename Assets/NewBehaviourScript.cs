38b29aa0fb4dbb55e628cf0e22bb79ff66cf8518
timliau <timothyliaukq@gmail.com>
2021-09-09 13:28:23 +0800

    Amended First Commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+	Transform[] waypoints;
+
+	[SerializeField]
+	float moveSpeed = 2f;
+
+	int waypointIndex = 0;
+
+	void Start() {
+		print("Start");
+		transform.position = waypoints [waypointIndex].transform.position;
+	}
+
+	void Update() {
+		print("Update");
+		Move();
+	}
+
+	void Move()
+	{
+		print("Move");
+		transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+
+		if (transform.position == waypoints [waypointIndex].transform.position) {
+			waypointIndex += 1;
+		}
+				
+		if (waypointIndex == waypoints.Length)
+			waypointIndex = 0;
+	}
+}
